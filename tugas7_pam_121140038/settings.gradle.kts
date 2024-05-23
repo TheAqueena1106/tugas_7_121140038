64fe8cea21c8f7c4022bf1f510cf73478cae6e48
The Aqueena <ernitafighter@gmail.com>
2024-05-24 03:07:03 +0700

    tugas 5

---
+++ tugas7_pam_121140038/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "tugas7_pam_121140038"
+include(":app")
+ 
